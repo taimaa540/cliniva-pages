272d2fd631e44b805679f785bcef3ea415fd4630
BasheerRaj <developer06.arabinterface@gmail.com>
2025-09-04 09:40:54 +0300

    Wrap MainApp in I18nextProvider for internationalization support

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./i18n";
 import "react-phone-input-2/lib/style.css";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 import { ElementUsersNoDataTo } from "./screens/ElementUsersNoDataTo/ElementUsersNoDataTo";
 import { ElementUsersAddNew } from "./screens/ElementUsersAddNew/ElementUseresAddNew";
 import { ElementUsersDesktop } from "./screens/UsersDesktop/ElementUsersDesktop";
@@ -79,6 +81,8 @@ function MainApp() {
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <MainApp />
+    <I18nextProvider i18n={i18n}>
+      <MainApp />
+    </I18nextProvider>
   </StrictMode>
 );
